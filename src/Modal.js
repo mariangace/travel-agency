9d4db842a0fdfcc7ef68ca66b26cb3c4b1ad93dc
Kuei-Chin Huang <kuei-chin.huang@mygeorgian.ca>
2020-12-28 09:28:13 -0500

    ADD: complete clickPutside function in modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,23 +1,23 @@
-import React, { useRef} from 'react';
-import"./Modal.css";
+import React, { useRef } from 'react'
+import './Modal.css'
 
 function Modal(props) {
-    const myRef = useRef();
+  const myRef = useRef()
 
-    const clickOutside = (e) => {
-        console.log(e);
-        props.clickOutside();
+  const clickOutside = (e) => {
+    if (!myRef.current.contains(e.target)) {
+      props.clickOutside()
     }
+  }
 
-    return (
-        
-        <div className="modal" onClick={clickOutside}>
-            <div className="modal__content" ref={myRef}>
-                <h1> YEP </h1>
-                <p> This a p tag </p>
-            </div>
-        </div>
-    )
+  return (
+    <div className='modal' onClick={clickOutside}>
+      <div className='modal__content' ref={myRef}>
+        <h1> YEP </h1>
+        <p> This a p tag </p>
+      </div>
+    </div>
+  )
 }
 
 export default Modal
