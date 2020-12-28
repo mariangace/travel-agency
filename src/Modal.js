b04930709feef39ad7926abaca27dd5d1ac2cf9c
Mariana García <mariangace@gmail.com>
2020-12-28 13:06:45 -0600

    adding contact form to modal

--- src/Modal.js
+++ src/Modal.js
@@ -13,8 +13,18 @@ function Modal(props) {
   return (
     <div className='modal' onClick={clickOutside}>
       <div className='modal__content' ref={myRef}>
-        <h1> YEP </h1>
-        <p> This a p tag </p>
+        <h1> Login </h1>
+        <form className="modal__form">
+            <label>
+                Email
+              <input type="email" name="email" placeholder='hi'/>
+            </label>
+            <label>
+                Password
+                <input type="password"/> 
+            </label>
+            <input className="submit__button" type='submit' value="Submit" /> 
+        </form>
       </div>
     </div>
   )
