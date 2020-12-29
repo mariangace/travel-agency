1c766b1500291a7380a414897678315eecbbe488
Mariana García <mariangace@gmail.com>
2020-12-29 17:50:31 -0600

    adding app to de env file

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import firebase from 'firebase';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB-ipcqXVIpo_7xSCqOQLbgpw990VIlpjg",
+    apiKey: process.env.REACT_APP_DB_KEY,
     authDomain: "travel-agency-5847a.firebaseapp.com",
     projectId: "travel-agency-5847a",
     storageBucket: "travel-agency-5847a.appspot.com",
@@ -12,6 +12,7 @@ const firebaseConfig = {
     measurementId: "G-KPT7WV1XPK"
   };
 
+  console.log(firebaseConfig)
   const firebaseApp = firebase.initializeApp(firebaseConfig);
 
   const db = firebaseApp.firestore();
